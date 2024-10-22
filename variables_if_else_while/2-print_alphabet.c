c1b2e33e855e8ff78e31e7ce57e40387e89926dd
JaRenie-spec <renie.jeanalain@gmail.com>
2024-10-22 11:27:53 +0000

    alphabet

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -8,9 +8,19 @@
 
 
 int main(void)
-
 {
+	/**
+	 * Declare a variable with the first member of the range [a-z]
+	 */
+
 	char alphabet;
+
+	/**
+	 *check to see if the current value of your variable
+	 *is less thanor the same as the last member (z)
+	 *of the given range. While the value is less
+	 *go ahead and print the value
+	*/
 
 for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 {

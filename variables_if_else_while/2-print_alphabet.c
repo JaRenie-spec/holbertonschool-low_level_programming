df535054275191323940569a1bf9fe07956aed51
JaRenie-spec <renie.jeanalain@gmail.com>
2024-10-22 11:29:42 +0000

    alphabet

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
 /**
- *printing the alphabet in lower case
+ *printing the alphabet in lower case.
  *
- * return: always 0
+ * return: always 0.
  */
 
 
 int main(void)
 {
 	/**
-	 * Declare a variable with the first member of the range [a-z]
+	 * Declare a variable with the first member of the range [a-z].
+
 	 */
 
 	char alphabet;
@@ -19,7 +20,7 @@ int main(void)
 	 *check to see if the current value of your variable
 	 *is less thanor the same as the last member (z)
 	 *of the given range. While the value is less
-	 *go ahead and print the value
+	 *go ahead and print the value.
 	*/
 
 for (alphabet = 'a'; alphabet <= 'z'; alphabet++)

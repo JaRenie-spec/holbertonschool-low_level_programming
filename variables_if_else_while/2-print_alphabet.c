dab284cfcf24a6ea57c5ce9b460dc96639e22f09
JaRenie-spec <renie.jeanalain@gmail.com>
2024-10-22 11:06:30 +0000

    alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/*printing the alphabet in lower case*/
+
+
+int main(void)
+
+{
+	char alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+	putchar(alphabet);
+}
+
+putchar('\n');
+
+return (0);
+
+}

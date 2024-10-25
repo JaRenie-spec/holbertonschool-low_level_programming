538625d438d26a9a46ecbbcfd269a3fff5c9b149
JaRenie-spec <renie.jeanalain@gmail.com>
2024-10-25 13:29:22 +0000

    a+b

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *add - function that adds two integers and returns the result
+ *@a: integer a
+ *@b: integer b
+ * Return: 0 success
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}

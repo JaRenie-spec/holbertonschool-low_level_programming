1ff2eb7309e95235b179965de80b683a502c02b8
JaRenie-spec <renie.jeanalain@gmail.com>
2024-10-28 13:38:50 +0000

    print line

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -8,12 +8,19 @@
 
 void print_line(int n)
 {
+	int l;
+
 	if (n <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		_putchar('_');
+		for (l = 1; l <= n; l++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+
 	}
 }

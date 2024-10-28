f29495c160fc0418623e2e009ef632e3ec706b1b
JaRenie-spec <renie.jeanalain@gmail.com>
2024-10-28 12:16:11 +0000

    numbers x10

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - print ten times the numbers
+ * from 0 to 14 followed by a new line.
+ *
+ */
+
+void more_numbers(void)
+{
+	int n;
+	int num;
+
+	while (n <= 9)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+			if ( num > 9)
+				_putchar((num / 10) + '0');
+			_putchar((num % 10) + '0');
+		}
+
+		_putchar('\n');
+
+		n++;
+	}
+}

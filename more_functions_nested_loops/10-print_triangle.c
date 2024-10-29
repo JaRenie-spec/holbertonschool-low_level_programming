c16cb7e2752908ecb714256441fd509974931089
JaRenie-spec <renie.jeanalain@gmail.com>
2024-10-29 10:35:00 +0000

    triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_triangle - print a triangle
+ *
+ */
+
+void print_triangle(int size)
+{
+	int n, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (n = 0; n < size; n++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				if (j < size - n - 1)
+				_putchar(' ');
+				else
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+}

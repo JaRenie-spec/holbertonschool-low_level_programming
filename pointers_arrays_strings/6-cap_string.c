e07a567a227a565988d63ccb31621a3674e69d2d
JaRenie-spec <renie.jeanalain@gmail.com>
2024-11-01 12:42:59 +0000

    cap string

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * cap_string - function to capitalizes.
+ * @str: the string
+ * Return: str
+ */
+
+char *cap_string(char *str)
+{
+	int i, n;
+
+	char comp[13] = {' ', '\t', '\n', ',', ';', '.',
+'!', '?', '"', '(', ')', '{', '}'};
+
+	for (i = 0 ; str[i] != '\0'; i++)
+	{
+		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] -= 32;
+		}
+		for (n = 0; n < 13; n++)
+		{
+			if (str[i] == comp[n])
+			{
+				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+				{
+					str[i + 1] -= 32;
+				}
+			}
+		}
+	}
+		return (str);
+}

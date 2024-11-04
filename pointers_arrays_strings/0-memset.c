298d2103bfec0da0f5e10e94d5620797367e973b
JaRenie-spec <renie.jeanalain@gmail.com>
2024-11-04 09:02:42 +0000

    memset

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - function that fills memory with a constant byte
+ * @s: the pointed area
+ * @b: the constant
+ * @n: the size
+ * Return: a pointer to the memory area s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; n > 0; i++)
+	{
+		s[i] = b;
+		n -= 1;
+	}
+	return (s);
+}

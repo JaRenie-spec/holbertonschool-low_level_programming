bd740605ec2cb3c47697376f2f1fca12454507b6
JaRenie-spec <renie.jeanalain@gmail.com>
2024-11-04 12:37:48 +0000

    strchr

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  * _strchr - function that locates a character in a string.
@@ -17,5 +18,5 @@ char *_strchr(char *s, char c)
 		}
 		s++;
 	}
-	return (0);
+	return (NULL);
 }

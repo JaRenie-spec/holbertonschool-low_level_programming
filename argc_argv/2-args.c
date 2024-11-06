92f6a81b49ad6500230b404f7a53343027947ad5
JaRenie-spec <renie.jeanalain@gmail.com>
2024-11-06 14:49:38 +0100

    print argument receives

---
+++ argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print all arguments
+ * @argc: int
+ * @argv: list arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf ("%s\n", argv[i]);
+	}
+	return (0);
+}

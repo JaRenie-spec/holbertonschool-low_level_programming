d1641d6d9e6cbc391603a310a7b7ad7353bb9e5a
JaRenie-spec <renie.jeanalain@gmail.com>
2024-11-07 12:53:07 +0100

    betty

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -5,7 +5,7 @@
  * _strchr - function that locates a character in a string.
  * @s: the first occurence of the character
  * @c: the string
- * Return: s or 0 if the character is not found.
+ * Return: s or NULL if the character is not found.
  */
 
 char *_strchr(char *s, char c)

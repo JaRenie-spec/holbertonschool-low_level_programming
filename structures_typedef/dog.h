7f415b1b4663227f8589a711e6dd80d3c4e24ba9
JaRenie-spec <renie.jeanalain@gmail.com>
2024-11-14 14:19:59 +0100

    betty change

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,17 @@
+#ifndef dog.h
+#define dog.h
+
+/**
+ * struct dog - define new type
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+#endif

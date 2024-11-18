a9a3cf5865d371aabc876ddc5d07a31561293295
JaRenie-spec <renie.jeanalain@gmail.com>
2024-11-18 10:55:25 +0100

    print name

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - functin that prints name
+ * @f: function pointer
+ * @name: the function
+ *
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f == 0)
+	{
+		return;
+	}
+	f(name);
+}

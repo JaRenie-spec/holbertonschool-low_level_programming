3c81dac28876276d6fd942f06a2266ab3868e260
JaRenie-spec <renie.jeanalain@gmail.com>
2024-11-18 11:59:27 +0100

    iterator in a array

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+#include <stddef.h>
+
+/**
+ * array_iterator - function given as parameter on each element of an array
+ * @size: the size of the array
+ * @action: pointer to the function.
+ *
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (size == 0)
+	{
+		return;
+	}
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}

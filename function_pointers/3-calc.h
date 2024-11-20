16dc4306b6597ad8fb2b9d8ce120b857cd19d36a
JaRenie-spec <renie.jeanalain@gmail.com>
2024-11-20 10:52:10 +0100

    correction

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -1,15 +1,15 @@
-#ifndef op_h
-#define op_h
+#ifndef _op_h
+#define _op_h
 
 typedef struct op
 {
 	char *op;
 	int (*f)(int a, int b);
 } op_t;
-	int op_add(int a, int b);
-	int op_sub(int a, int b);
-	int op_mul(int a, int b);
-	int op_div(int a, int b);
-	int op_mod(int a, int b);
-	int (*get_op_func(char *s))(int, int);
+int (*get_op_func(char *s))(int, int);
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
 #endif
--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -22,9 +22,9 @@ int (*get_op_func(char *s))(int, int)
 
 	int i = 0;
 
-	while (ops[i].op != NULL)
+	while (i < 5)
 	{
-		if (*(ops[i].op) == *s)
+		if (strcmp(s, ops[i].op) == 0)
 		{
 			return (ops[i].f);
 		}

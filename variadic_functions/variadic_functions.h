7b07ecb9a79c10bd1123513e0c76a232ca972440
JaRenie-spec <renie.jeanalain@gmail.com>
2024-11-20 13:46:17 +0100

    guard up

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,5 +1,5 @@
-#ifndef Robin_h
-#define Robin_h
+#ifndef _ROBIN_h
+#define _ROBIN_h
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);

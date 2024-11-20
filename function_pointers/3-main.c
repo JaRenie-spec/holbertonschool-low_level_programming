291e47f59e21733311a7685321509de63481af92
JaRenie-spec <renie.jeanalain@gmail.com>
2024-11-20 10:15:21 +0100

    correction

---
+++ function_pointers/3-main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "3-calc.h"
+#include <stdlib.h>
+
+/**
+ * @brief
+ *
+ * @return int
+ */
+
+int main(int argc, char *argv[])
+{
+	char *op;
+	int calc;
+	int num1, num2;
+
+	if (argc > 4)
+	{
+		printf("error\n");
+		exit (98);
+	}
+	if (argv[2] != op)
+	{
+		printf("error\n");
+		exit (99);
+	}
+	printf("%d\n", calc);
+}

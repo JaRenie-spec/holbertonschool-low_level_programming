b29a47514685f92059106fac8decc97b54ed136b
JaRenie-spec <renie.jeanalain@gmail.com>
2024-11-21 12:07:17 +0100

    correction output

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -12,14 +12,19 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i;
+	int i = 0;
 
-	if (size <= 0)
-	
-	for (i = 0; i < size; i++)
-
-		if (cmp(array[i]) != 0)
-			return (i);
-
+	if (size > 0)
+	{
+		if (array != NULL && cmp != NULL)
+		{
+			while (i < size)
+			{
+				if (cmp(array[i]))
+					return (i);
+				i++;
+			}
+		}
+	}
 	return (-1);
 }

302335acdd00d034b0a3cc53ee5c236ba1dbb240
JaRenie-spec <renie.jeanalain@gmail.com>
2024-11-21 11:07:17 +0100

    correction output

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -13,27 +13,34 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list ap;
 	unsigned int i;
-	char *s;
+	char *str;
 
 	if (n == 0)
+	{
 		printf("\n");
 		return;
+	}
 
 	va_start(ap, n);
 
 	for (i = 0; i < n; i++)
 	{
-		s = va_arg(ap, char *);
+		str = va_arg(ap, char *);
 
-		if (s == NULL)
-			printf("nil");
-
+		if (str == NULL)
+		{
+			printf("(nil)");
+		}
 		else
-			printf("%s", s);
+		{
+			printf("%s", str);
+		}
 
 		if (separator != NULL && i != n - 1)
+		{
 			printf("%s", separator);
+		}
 	}
-	printf("\n");
-	va_end(ap);
+		printf("\n");
+		va_end(ap);
 }

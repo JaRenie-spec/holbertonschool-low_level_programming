97a325f016dcbfb81cb92764f2be8066421d66e2
JaRenie-spec <renie.jeanalain@gmail.com>
2024-11-21 13:21:21 +0100

    betty change

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -4,12 +4,11 @@
  * print_name - functin that prints name
  * @f: function pointer
  * @name: the name to be printed
- * Return: nothing
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (f == NULL || name == NULL)
+	if (name == NULL || f == NULL)
 	{
 		return;
 	}

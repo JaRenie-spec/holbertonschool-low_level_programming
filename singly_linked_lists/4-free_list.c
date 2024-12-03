40d07e22654a5b580f132a669ef6d60e099df450
JaRenie-spec <renie.jeanalain@gmail.com>
2024-12-03 10:27:39 +0100

    free node

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * @brief
+ *
+ */
+
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp->str);
+		free(tmp);
+	}
+}

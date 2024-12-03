bd89cb20d7cb4c0934f2b55556ca8b208eaf1dc8
JaRenie-spec <renie.jeanalain@gmail.com>
2024-12-03 13:46:08 +0100

    modification betty

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,7 +1,6 @@
 #ifndef GUARD_h
 #define GUARD_h
 
-
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -14,9 +13,9 @@
 
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+char *str;
+unsigned int len;
+struct list_s *next;
 } list_t;
 
 int _putchar(char c);

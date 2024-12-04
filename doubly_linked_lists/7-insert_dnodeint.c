d54e17ba4b921003b7850fba9027af4c8dd013b2
JaRenie-spec <renie.jeanalain@gmail.com>
2024-12-04 13:39:18 +0100

    insert index

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,57 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - insert a new node at a given position
+ * @h: pointer to the first element of the list
+ * @idx: index to insert the new element
+ * @n: the value to insert.
+ * Return: the pointer to the new element or NULL if it failed
+ */
+
+
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new;
+	dlistint_t *head;
+	unsigned int i;
+
+	new = NULL;
+	if (idx == 0)
+		new = add_dnodeint(h, n);
+	else
+	{
+		head = *h;
+		i = 1;
+		if (head != NULL)
+			while (head->prev != NULL)
+				head = head->prev;
+
+		while (head != NULL)
+		{
+			if (i == idx)
+			{
+				if (head->next == NULL)
+					new = add_dnodeint_end(h, n);
+					
+				else
+				{
+					new = malloc(sizeof(dlistint_t));
+
+					if (new != NULL)
+					{
+						new->n = n;
+						new->next = head->next;
+						new->prev = head;
+						head->next->prev = new;
+						head->next = new;
+					}
+				}
+				break;
+			}
+			head = head->next;
+			i++;
+		}
+	}
+	return (new);
+}

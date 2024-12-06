18edf622647587fbeed239877c297893f870b94d
JaRenie-spec <renie.jeanalain@gmail.com>
2024-12-06 16:15:09 +0100

    correction while condition

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -32,15 +32,15 @@ int main(int argc, char *argv[])
 	}
 	while ((num = read(file_from, buffer, 1024)) > 0)
 	{
-		if (write(file_to, buffer, num) != num)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
-		}
 		if (num == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
+		}
+		if (write(file_to, buffer, num) != num)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
 		}
 	}
 	if ((close(file_from) == -1) && (close(file_to) == -1))

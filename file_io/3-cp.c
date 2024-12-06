feaa906e52551fd1169c3c906c8366dcdabe1412
JaRenie-spec <renie.jeanalain@gmail.com>
2024-12-06 16:12:35 +0100

    modified the while condition to implement the exit 98

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -37,11 +37,11 @@ int main(int argc, char *argv[])
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
-	}
-	if (num == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
+		if (num == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
 	}
 	if ((close(file_from) == -1) && (close(file_to) == -1))
 	{

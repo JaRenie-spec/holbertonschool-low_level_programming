bb24f68341ae6e28da1ec4497f70aa8f7f987d8f
JaRenie-spec <renie.jeanalain@gmail.com>
2024-12-06 16:02:21 +0100

    changing the close condition

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -43,7 +43,7 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	if (close(file_from) && close(file_to) == -1)
+	if ((close(file_from) == -1) && (close(file_to) == -1))
 	{
 	dprintf(STDERR_FILENO, "Error: Can't close fd %d, %d\n", file_from, file_to);
 	exit(100);
